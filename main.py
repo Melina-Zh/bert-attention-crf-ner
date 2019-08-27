1cd1fbb3bd4068f519c1a81dd36252b80fde7c97
MAO <maoqr@dell-mgt-01.ivic.org.cn>
2019-08-27 17:18:42 +0800

    five res

--- main.py
+++ main.py
@@ -305,7 +305,7 @@ def train(**kwargs):
             break
         save_model(model, epoch)
     time2 = time.time()
-    five_r=open("five_res.log", 'a')
+    five_r=open("five_res3.log", 'a')
     five_r.write("{:.4f}\n".format(acc))
     five_r.close()
     print("total time: {:.1f}s".format(time2-time1))
